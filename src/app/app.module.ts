b0d412182f8b4c3565ee72e22b27552a1972d76c
Danny Cárdenas <danny.cardenas@bayteq.com>
2024-11-07 17:00:28 -0500

    cap 330

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProductsModule } from './products/products.module';
+import { provideHttpClient } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,7 @@ import { ProductsModule } from './products/products.module';
     AppRoutingModule,
     ProductsModule
   ],
-  providers: [],
+  providers: [provideHttpClient()],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/signals/services/user-service.service.ts
@@ -0,0 +1,20 @@
+import { inject, Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { map, Observable, tap } from 'rxjs';
+import { SingleUserResponse, User } from '../interfaces/user-request.interface';
+
+@Injectable({ providedIn: 'root' })
+export class ServiceNameService {
+  public httpClient = inject(HttpClient);
+  constructor() { }
+
+  private baseURl = 'https://reqres.in/api/users'
+
+  getUserById(id: number): Observable<User> {
+    return this.httpClient.get<SingleUserResponse>(`${this.baseURl}/${id}`)
+      .pipe(
+        map(m => m.data),
+        tap(m => console.log(m))
+      )
+  }
+}
