28b763e54d9871455f932feb832fa257f39f6565
Danny Cárdenas <danny.cardenas@bayteq.com>
2024-11-07 20:45:57 -0500

    cap 335

--- src/app/signals/pages/properties-page/properties-page.component.ts
+++ src/app/signals/pages/properties-page/properties-page.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { User } from '../../interfaces/user-request.interface';
 
 @Component({
   selector: 'app-properties-page',
@@ -6,9 +7,24 @@ import { Component } from '@angular/core';
   styleUrl: './properties-page.component.css'
 })
 export class PropertiesPageComponent {
+  public user = signal<User>({
+    id: 2,
+    email: "janet.weaver@reqres.in",
+    first_name: "Janet",
+    last_name: "Weaver",
+    avatar: "https://reqres.in/img/faces/2-image.jpg"
+  })
 
-
-  onFieldUpdated(field: string, value: string) {
+  onFieldUpdated(field: keyof User, value: string) {
+    this.user.update(user => {
+      switch (field) {
+        case 'id': user.id = Number(value); break;
+        case 'email': user.email = value; break;
+        case 'first_name': user.first_name = (value); break;
+        case 'last_name': user.last_name = (value); break;
+      }
+      return user;
+    })
   }
 
 }
